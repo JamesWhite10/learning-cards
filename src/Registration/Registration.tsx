cfc6343ce78dc004fc9a32e8508f06f8a6fc686c
JamesWhite10 <max.migalin10@gmail.com>
2021-08-21 20:50:56 +0300

    Refactoring Registration component.

--- src/Registration/Registration.tsx
+++ src/Registration/Registration.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import {NavLink} from "react-router-dom";
+import {PATH} from "../Main/Routes/Routes";
+import s from "../Main/Header/Header.module.css";
 
 export const Registration = () => {
     return (
         <div>
-
+            <div><input type="text"/></div>
+            <div><input type="text"/></div>
+            <div><input type="text"/></div>
+            <div><button>sign up</button></div>
+            <div>
+                <NavLink to={PATH.LOGIN} className={s.link} activeClassName={s.active}>Login</NavLink>
+            </div>
         </div>
     )
 }
