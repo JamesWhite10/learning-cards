2b1508daee393115e29dc515d763f1180659a53e
Nikita <knanaprimer@gmail.com>
2021-08-23 01:09:20 +0300

    test from nik

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 import {TestComponent} from "../Test/Test";
 import Main from "../Main/Main";
 
-
+//Hello team! testing git branches
 function App() {
     return (
         <div className="App">
