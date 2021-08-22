619c4922434b6e6a05b482044c79bf3792d536f7
JamesWhite10 <max.migalin10@gmail.com>
2021-08-22 20:21:31 +0300

    Layout and design of the navbar.

--- src/Main/Header/Header.tsx
+++ src/Main/Header/Header.tsx
@@ -2,14 +2,16 @@ import React from 'react'
 import {NavLink} from 'react-router-dom'
 import s from "./Header.module.css"
 import {PATH} from "../Routes/Routes";
+import menu from "../../media/menu/menu.png"
 
 function Header() {
     return (
         <div className={s.header}>
             <NavLink to={PATH.PROFILE} className={s.link} activeClassName={s.active}>Profile</NavLink>
-            <NavLink to={PATH.LOGIN} className={s.link} activeClassName={s.active}>Login</NavLink>
+            <NavLink to={PATH.LOGIN} className={s.link} activeClassName={s.active}>Entrance</NavLink>
             <NavLink to={PATH.REGISTRATION} className={s.link} activeClassName={s.active}>Registration</NavLink>
-            <div className={s.block}/>
+            <NavLink to={PATH.RECOVERY} className={s.link} activeClassName={s.active}>Password recovery</NavLink>
+            <div><img src={menu} className={s.block} alt={menu}/></div>
         </div>
     )
 }
