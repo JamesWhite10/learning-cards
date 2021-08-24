6e762225234da5a44cfa60f797dd532d5796daac
JamesWhite10 <max.migalin10@gmail.com>
2021-08-24 16:11:55 +0300

    Refactor API.

--- src/api/api.ts
+++ src/api/api.ts
@@ -22,7 +22,29 @@ export const loginAPI = {
     }
 }
 
+export const PasswordRecoveryAPI = {
+    forgot(email: string) {
+        return instance.post<RecoveryResponseType>('auth/recovery', {
+            email,
+            from: 'test-front-admin <ai73a@yandex.by>',
+            message: `<div style="background-color: lime; padding: 15px">
+                            password recovery link: 
+                            <!--нужен путь к токену Максаб но я не врубаюсь пока что к чему-->
+                        <a href='https://JamesWhite10.github.io/learning-cards#/new-password/$token$'>link</a></div>`
+        })
+    }
+}
+
+
 //TYPES=====
+
+//recovery password
+type RecoveryResponseType = {
+    email: string
+    from: string
+    message: any
+}
+
 
 //registrationAPI
 type registrationResponseType = {
@@ -57,6 +79,6 @@ type loginResponseType = {
     error?: string
 }
 type logoutResponseType = {
-    info: 'logOut success —ฅ/ᐠ.̫ .ᐟ\ฅ—',
+    info: 'logOut success —ฅ/ᐠ.̫ .ᐟฅ—',
     error: string
 }
